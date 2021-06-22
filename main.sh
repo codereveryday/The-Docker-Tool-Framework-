fb8215bf38549675bc4d3581d5221b02cd90dc43
Coder_Everyday <67515633+codereveryday@users.noreply.github.com>
2021-06-22 16:32:05 -0500

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,20 +1,27 @@
  #!/bin/sh
- clear 
- printf '\033]2;INSTALLER\a'
+  printf '\033]2;INSTALLER\a'
+ echo -e              "─────▀▄▀─────▄─────▄      									
+                        ▄███████▄──▀██▄██▀
+                       ▄█████▀█████▄──▄█
+                      ███████▀████████▀    TDTF
+                       ─▄▄▄▄▄▄███████▀"
+ 
+ 
+ echo -e  "Welcome to TheDockerToolFramework"                  
  echo -e "Press\e[1;37m any key\e[1;37m to install the script..."
  read -n 1
 clear
-mkdir /root/TDTF
-cp -r "$DIR"/* /root/TDTF
-chmod +x /root/TDTF/main.sh
-gnome-terminal -- "bash /root/TDTF/main.sh"
+mkdir /home/TDTF 
+cp -r "$DIR"/* /home/TDTF
+chmod +x /home/TDTF/main.sh
+konsole -- "bash /home/TDTF/main.sh"
 fi
 echo -e "Installing TDTF TheDockerToolFramework..."
 sleep 1
 echo -e "Fixing permissions"
 sleep 2
-chmod +x /root/TDTF/main.sh
-chmod +x /root/TDTF/TDTF
+chmod +x /home/TDTF/main.sh
+chmod +x /home/TDTF/TDTF
 echo "Welcome To TheDockerToolFramework(TDTF)"
 echo "A Docker Project"
 sleep 4
@@ -30,5 +37,5 @@ sleep 1
 clear
 echo -e "Open a NEW terminal and type 'TDTF' to launch the script"
 sleep  4
-gnome-terminal -- TDTF
+konsole -- TDTF
 exit
